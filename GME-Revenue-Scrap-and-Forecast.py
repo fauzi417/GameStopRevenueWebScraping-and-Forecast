c2dde87e7c3a78415e48a72b21473424110cd238
Fauzi Achmad Ramadhan <85028034+fauzi417@users.noreply.github.com>
2021-09-04 10:46:09 +0700

    Update GME-Revenue-Scrap-and-Forecast.py

--- GME-Revenue-Scrap-and-Forecast.py
+++ GME-Revenue-Scrap-and-Forecast.py
@@ -109,7 +109,6 @@ print(hasil.summary())
 hasil.resid.plot(kind='kde')
 
 #model test
-
 gme_revenue['model test']=hasil.predict(start=55, end=66, dynamic=True)
 gme_revenue[['Revenue','model test']].plot()
 plt.show()
